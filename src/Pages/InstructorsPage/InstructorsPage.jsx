5c7a5e39519c74cf3f3171a53f82411a9f72d527
babunewgit1 <babunewx11@gmail.com>
2023-06-12 06:29:06 +0600

    ins page completed

--- src/Pages/InstructorsPage/InstructorsPage.jsx
+++ src/Pages/InstructorsPage/InstructorsPage.jsx
@@ -13,15 +13,18 @@ const InstructorsPage = () => {
   console.log(instractor);
 
   return (
-    <section id="instructors" className="md:py-24 py-8">
+    <section id="instructors" className="md:py-24 py-8 dark:bg-[#20252d]">
       <div className="mycontainer">
         <div className="courseheading">
           <Heading heading="Our Popular Instructors"></Heading>
         </div>
-        <div className="classWrapper grid grid-cols-3 gap-8">
+        <div className="classWrapper grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {instractor.map((items) => {
             return (
-              <div key={items._id} className="classBox">
+              <div
+                key={items._id}
+                className="classBox boxShadow rounded-lg overflow-hidden"
+              >
                 <div className="classImg">
                   <img
                     className="w-full h-[300px] mb-5 object-cover object-top"
@@ -29,8 +32,10 @@ const InstructorsPage = () => {
                     alt=""
                   />
                 </div>
-                <h3 className="text-3xl font-medium">{items.name}</h3>
-                <p>{items?.email}</p>
+                <div className="classDetails p-2 md:p-6 dark:text-white">
+                  <h3 className="text-3xl font-medium">{items.name}</h3>
+                  <p>{items?.email}</p>
+                </div>
               </div>
             );
           })}
